e19e19ad9ec97332e8c4aa629100b7db3064d849
Aaron Siegel <datadreamerlabs@gmail.com>
2015-06-10 21:15:18 -0700

    Reducing unnecessary lines.

--- youtube.py
+++ youtube.py
@@ -1,26 +1,15 @@
 #!/home/asiegel/dev/bin/python
 
-import cgi
-import pafy
-import urllib
-import sys
-
+import sys, cgi, urllib, pafy
 print "Content-Type: video/mp4\n"
-
 BUFFER_SIZE = 16 * 1024
-
 form = cgi.FieldStorage()
-id = form["id"].value
-url = "https://www.youtube.com/watch?v=" + id
+url = "https://www.youtube.com/watch?v=" + form["id"].value
 video = pafy.new(url)
 best = video.getbest()
-
 req = urllib.urlopen(best.url)
 while 1:
 	chunk = req.read(BUFFER_SIZE)
 	if not chunk:
 		break
 	sys.stdout.write(chunk)
-
-
-
